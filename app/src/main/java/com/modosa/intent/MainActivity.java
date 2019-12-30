d966f1984a7184e565b6ad5c0a2d96a3cfc358c3
dadaewqq <dadaewqq@gmail.com>
2019-12-30 14:15:54 +0800

    优化代码结构

--- app/src/main/java/com/modosa/intent/MainActivity.java
+++ app/src/main/java/com/modosa/intent/MainActivity.java
@@ -6,16 +6,27 @@ import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Toast;
 
 import java.util.Objects;
+
+/*
+res\values\strings.xml
+<resources>
+<string name="app_name">IntentDemo</string>
+<string name="ComponentName_pkg">com.android.settings</string>
+<string name="ComponentName_class">com.android.settings.Settings</string>
+</resources>
+
+intent.setComponent(new ComponentName(getString(R.string.ComponentNamepkg_), getString(R.string.ComponentName_class)));
+*/
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent("android.intent.action.MAIN");
-        intent.addCategory("android.intent.category.LAUNCHER");
 
         ApplicationInfo appInfo = null;
         try {
@@ -24,20 +35,26 @@ public class MainActivity extends Activity {
             e.printStackTrace();
         }
         Objects.requireNonNull(appInfo);
-        String _pkg=appInfo.metaData.getString("ComponentName_pkg");
-        String _cls=appInfo.metaData.getString("ComponentName_class");
-        intent.setComponent(new ComponentName(_pkg, _cls));
-        /*
-        res\values\strings.xml
-        <resources>
-        <string name="app_name">IntentDemo</string>
-        <string name="ComponentName_pkg">com.android.settings</string>
-        <string name="ComponentName_class">com.android.settings.Settings</string>
-        </resources>
+        String Pkg = appInfo.metaData.getString("ComponentName_pkg");
+        String Cls = appInfo.metaData.getString("ComponentName_class");
+        if (Pkg == null || Cls == null || TextUtils.isEmpty(Pkg.trim()) || TextUtils.isEmpty(Cls.trim())) {
+            Toast.makeText(this, "Empty !", Toast.LENGTH_SHORT).show();
+        } else {
 
-        intent.setComponent(new ComponentName(getString(R.string.ComponentName_pkg), getString(R.string.ComponentName_class)));
-        */
-        startActivity(intent);
-        finish();
+//            Intent intent = new Intent("android.intent.action.MAIN");
+//            intent.addCategory("android.intent.category.LAUNCHER");
+
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setComponent(new ComponentName(Objects.requireNonNull(Pkg), Objects.requireNonNull(Cls)));
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            try {
+                startActivity(intent);
+            } catch (Exception e) {
+//            e.printStackTrace();
+                Toast.makeText(this, e + "", Toast.LENGTH_SHORT).show();
+            }
+            finish();
+        }
+
     }
 }
